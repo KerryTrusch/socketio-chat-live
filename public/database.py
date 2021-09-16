0ae375d6adf250bc24876e410344f1c0f4d310e1
KerryTrusch <kerry.trusch@gmail.com>
2021-09-16 19:48:15 -0400

    new python database

--- public/database.py
+++ public/database.py
@@ -1,2 +1,52 @@
 import json
 import os
+
+class canvasDB:
+    def __init__(self, location):
+        self.location = os.path.expanduser(location)
+        self.load(self.location)
+
+    def load(self, location):
+        if os.path.exists(location):
+            self._load()
+        else:
+            self.db = {}
+        return True
+
+    def _load(self):
+        self.db = json.load(open(self.location, "r"))
+
+    def dumpdb(self):
+        try:
+            json.dump(self.db, open(self.location, "w+"))
+            return True
+        except:
+            return False
+
+    def set(self, key, value):
+        try:
+            self.db[str(key)] = value
+            self.dumpdb()
+            return True
+        except Exception as e:
+            print("Error saving database values: " + str(e))
+            return False
+    
+    def get(self, key):
+        try:
+            return self.db[key]
+        except KeyError:
+            print("No value can be found for " + str(key))
+            return False
+
+    def delete(self, key):
+        if not key in self.db:
+            return False
+        del self.db[key]
+        self.dumpdb()
+        return True
+
+    def resetdb(self):
+        self.db={}
+        self.dumpdb()
+        return True
