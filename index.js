7fcf2affe8c2e75e67ae03dda80d1119e2bac6c4
KerryTrusch <kerry.trusch@gmail.com>
2021-09-16 23:00:40 -0400

    mongo

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 //https://socket.io/docs/v4/server-application-structure/
-//https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
-// ^ if I want to go the redux route of using states to control the canvas
+//https://github.com/socketio/socket.io/blob/master/examples/chat/index.js#L68
 let express = require('express');
-let socket = require("socket.io");
-let app = express();
-let server = app.listen(3000);
+let mongo   = require('mongodb').MongoClient;
+let socket  = require("socket.io");
+let app     = express();
+let server  = app.listen(4000);
 
 app.use(express.static('public'));
 
-let io = socket(server);
-io.sockets.on('connection', newConnection);
+mongo.connect('mongodb://127.0.0.1/Bitcanvas', function(err, db){
+    if(err){
+        throw err;
+    }
+    console.log('connected');
+});
 
-function newConnection(socket) {
-    console.log("new connection: " + socket.id);
-}
+let io = socket(server);
+io.on('connection', (socket) => {
+    
+});
