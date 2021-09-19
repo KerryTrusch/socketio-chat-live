98dd99c356fed85712429af3f73894e75b777764
KerryTrusch <kerry.trusch@gmail.com>
2021-09-19 00:07:35 -0400

    html and css framework

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ let app     = express();
 let server  = app.listen(4000);
 let messages = {};
 app.use(express.static('public'));
-
+numUsers = 1;
 let io = socket(server);
 io.on('connection', (socket) => {
-    socket.on("pixelPlaced", (data) => {
-        canvas[data.loc[0]][data.loc[1]] = data.COLOR;
-        socket.broadcast.emit("updateBoard", canvas);
+    socket.on("userJoin", () => {
+        numUsers++;
+        socket.emit("pastMessages", messages);
+    });
+    console.log(numUsers);
+    socket.on("disconnect", () => {
+        numUsers--;
+    });
+
+    socket.on("getMessage", (text, user, time) => {
+        if (Object.keys(messages).length >= 100) {
+            delete messages[0];
+            for (let i = 0; i < 100; i++) {
+                messages[i] = messages[i+1];
+                delete messages[i+1];
+            }
+        }
+        messages[Object.keys(message).length] = {"text": text, "user": user, "time": time};
     });
 });
--- public/src/user.js
+++ public/src/user.js
@@ -1,7 +1,22 @@
-let socket;
-socket = io.connect("http://localhost:3000");
-io.on('connection', (socket) => {
-    
-});
+$(function(){
+    const socket = io();
+    const colors = [
+        "#FFFFFF",
+        "#E4E4E4",
+        "#888888",
+        "#222222",
+        "#FFA7D1",
+        "#E50000",
+        "#E59500",
+        "#A06A42",
+        "#E5D900",
+        "#94E044",
+        "#02BE01",
+        "#00D3DD",
+        "#0000EA",
+        "#820080"
+    ];
 
+    socket.emit("userJoin", "username");
+});
 
