4aceeee63c91c054e0086d04462312b61c4dedd5
Trusch <41238059+KerryTrusch@users.noreply.github.com>
2021-10-05 22:44:22 -0400

    added files for server browser

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 let server = app.listen(port);
 let messages = [];
 let users = {};
-
+let room = "global_room";
 //Setting up mongoDB using mongoose
 const uri = process.env.MONGODB_URI || "mongodb+srv://dbAdmin:B2f9bxS%40t7%40i93j@cluster0.rcn3r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 main().catch(err => console.log(err));
@@ -23,18 +23,24 @@ const messageSchema = new mongoose.Schema({
     color: String,
     imgsrc: String
 });
-const message = mongoose.model('Message', messageSchema, 'message');
-message.find({}).exec(function (err, models) {
-    if (err) {
-        console.log(err);
-    } else {
-        (async function () {
-            for await (let key of models) {
-                messages.push(key);
-            }
-        })();
-    }
-});
+const message = mongoose.model('Message', messageSchema, room);
+
+//Message prepopulation done async to prevent users that join quickly from not recieving data
+function updateMessageStorage() {
+    message.find({}).exec(function (err, models) {
+        if (err) {
+            console.log(err);
+        } else {
+            messages = [];
+            (async function () {
+                for await (let key of models) {
+                    messages.push(key);
+                }
+            })();
+        }
+    });
+}
+updateMessageStorage();
 //Routing app to files in the public folder
 app.use(express.static('public'));
 
@@ -49,7 +55,7 @@ io.on('connection', (socket) => {
 
     io.emit("history", { "messages": messages, "users": users, "numUsers": numUsers });
 
-    socket.on("new user", async (uname) => {
+    socket.on("new user", (uname) => {
         users[uname["id"]] = uname["uname"];
         io.emit("user joined", { "newName": uname["uname"], "userId": uname["id"] });
     });
@@ -61,16 +67,7 @@ io.on('connection', (socket) => {
         io.emit("disconnected user", socket.id);
     });
 
-    //I only want to store 100 messages at a time so I pop the bottom element if we are at 100
     socket.on("getMessage", (data) => {
-        // if (Object.keys(messages).length >= 100) {
-        //     delete messages[0];
-        //     for (let i = 0; i < 100; i++) {
-        //         messages[i] = messages[i + 1];
-        //         delete messages[i + 1];
-        //     }
-        // }
-        // messages[Object.keys(messages).length] = { "text": data["text"], "user": data["user"], "time": data["time"], "color": data["color"], "imgsrc": data["imgsrc"] };
         const newMessage = new message({
             text: data["text"],
             user: data["user"],
